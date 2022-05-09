a7ceb35ab5bd4cf6144513d063b23eee5210b90f
Akamichy <yanixjunior1@gmail.com>
2022-05-09 20:44:52 +0300

    urok

--- 003_urok.py
+++ 003_urok.py
@@ -1,6 +1,58 @@
-price = 500.12333
-result = 'Price is {:.2f}'
-print(result.format(float(price)))
+user_id = input('Please enter id code: ')
+if len(user_id) !=11:
+    if len(user_id) > 11:
+        print('Code is too long')
+    else:
+        print('Code is too short')
+else:
+    if user_id.isdigit():
+        print(f'Your ID is {user_id}')
+    else:
+        print('You have to use numbers only')
+# user_input = input('Please input:')
+# if user_input == '1':
+#     print('1')
+# elif user_input == '2':
+#     print('2')
+# elif user_input == '3':
+#     print('3')
+# else:
+#     print('Choice is out of range')
+
+# x = 'Hello world'
+# if 'world' in x:
+#     print('greater than 100')
+# elif x == 150:
+#     print('X is 150')
+# elif x != 150:
+#     print('X is not 150')
+# else:
+#     print('Hello world')
+
+
+
+#если все условия провалились, то else выполняет в таком случае выбранную команду (exmpl:если все условия провалились, напиши привет мир)
+#если условие if проверилось, остальные не проверяются, игнорируются
+# x = 0
+# x += 1
+# print(x)
+# x += 5
+# print(x)
+# x == y - equal (False)
+# x! = y - not equal (true)
+
+# price = 500
+# name = 'john'
+# surname = 'smith'
+# age = '35'
+# result = f'Hello {name.capitalize()} {surname.capitalize()}. You are {age} years old. Your salary is {price:.2f}$.' #- автоматом поставило заглавные буквы у слова с помощью capitalize
+# print(result)
+# byte_sting = b'\xcf\x84o\xcf\x81\xce\xbdo\xcf\x82'
+# print(byte_sting.decode('utf-16'))
+# sample = 'Hello world'
+# print(sample.encode('utf-8'))
+# result = 'Price is {:.2f}'
+# print(result.format(float(price)))
 
 
 # print(round(1.5))
