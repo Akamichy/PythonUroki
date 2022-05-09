5f4659cc0a4546743cd607a0890f5fb2229bb305
Akamichy <yanixjunior1@gmail.com>
2022-05-09 19:23:34 +0300

    urok

--- 003_urok.py
+++ 003_urok.py
@@ -2,6 +2,16 @@ string_sample = "Hello world world"
 string_sample2 = "first letteR is lowErcase"
 string_sample3 = "extra whitespace string "
 
+# x = 'hello world'.split() #возвращает список, массив, разделяя после каждого после делителя(по стандарту пробел)
+# print(x)
+
+# name, surname = input('Please enter your Name and Surname ').split(' ')
+# print(name)
+# print(surname)
+
+# x = "hel\"lo" - "\\" один такой слэш означает окончание строки
+# print(string_sample.find("world", 7)) - считает с какого символа (по порядку) находит выбранное слово
+# print(string_sample.count("world")) - считает сколько раз найдено слово
 # print(string_sample[-7:-1])
 # x = string_sample[3]
 # print(x)
@@ -14,17 +24,15 @@ string_sample3 = "extra whitespace string "
 # print(string_sample[16])
 # print(type(len(string_sample))) - class int!!!!!
 # print(string_sample in string_sample) - clas bool
-
-print(string_sample2.upper())
-# print(string_sample2.isupper())
-print(string_sample3.lower())
-# print(string_sample3.islower())
-print(string_sample3.casefold())
-# user_entry = input().lower
-
-print(string_sample2.capitalize())
-print(string_sample2.title())
-string_sample = ("********* kfbksdfbsdfbb ****")
-print(string_sample.strip("* "))
-print(string_sample.rstrip("*"))
-print(string_sample.lstrip("*"))
+# print(string_sample2.upper()) все буквы капсом
+# # print(string_sample2.isupper()) тру/фолс
+# print(string_sample3.lower()) делает все буквы в переменной маленькими
+# # print(string_sample3.islower()) тру/фолс
+# print(string_sample3.casefold())
+# # user_entry = input().lower
+# print(string_sample2.capitalize())
+# print(string_sample2.title()) все первые буквы в слове заглавные
+# string_sample = ("********* kfbksdfbsdfbb ****")
+# print(string_sample.strip("* ")) убирает выбранный символ из переменной, не меняя ее тип
+# print(string_sample.rstrip("*"))
+# print(string_sample.lstrip("*"))
