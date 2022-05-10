76640a8e19cfc3aa464d9afe988fa4953096675b
Akamichy <yanixjunior1@gmail.com>
2022-05-10 18:22:14 +0300

    domaha

--- 002_domaha.py
+++ 002_domaha.py
@@ -1,6 +1,6 @@
 # Write a code to return "Hero" from given string
 example_string1 = "Hello bro"
-print(example_string1[0:2] + "ro")
+print(example_string1[0:2] + example_string1[7:9])
 
 # Write a code to return "Jack is my name"
 example_string2 = "jack Is My NAME"
@@ -13,16 +13,13 @@ print(example_string3.strip("*"))
 
 # Write a code to return "Hello my name is Jack"
 example_string4 = "hello my name is jack"
-part1 = "hello my name is"
-part2 = "jack"
-print((part1.capitalize()) + " " + (part2.capitalize()))
-
+print(example_string4[0:5].capitalize(), example_string4[5:16], example_string4[0:-4].capitalize())
 
 # Write a code to return formatted string "Hello, my name is Jack"
 var1 = "jack"
 var2 = "hello"
 var3 = "MY NAME IS"
-print(var2.capitalize() + ", " + (var3.lower()) + " " + (var1.capitalize()))
+print(f'{var2.capitalize()}, {var3.lower()} {var1.capitalize()}')
 
 
 # Write a code to return byte_string text value
@@ -31,8 +28,6 @@ print(byte_string.decode('utf-16'))
 print(byte_string.decode('utf-8'))
 # Write a code to return True if cost is greater than 500$
 example_string5 = "It cost me 1000.00$"
-x = 1000
-if x > 500:
+if float(example_string5[11:18]) > 500:
     print(True)
-else:
-    print (False)
+else: print(False)
