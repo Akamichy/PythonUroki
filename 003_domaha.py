09226b7d6e741e36dc8d8dc51a09a985e812bcee
Akamichy <yanixjunior1@gmail.com>
2022-05-11 19:14:57 +0300

    domaha

--- 003_domaha.py
+++ 003_domaha.py
@@ -20,12 +20,22 @@
 VARIANT 2
 
 '''
-# range = range(1, 101)
-# for num in range:
-#     if num % 15 == 0:
-#         print(num, 'FizzBuzz')
-#     elif num % 3 == 0:
-#         print(num, 'Fizz')
-#     elif num % 5 == 0:
-#         print(num, 'Buzz')
+range = range(1, 101)
+for num in range:
+    if num % 15 == 0:
+        print(num, 'FizzBuzz')
+    elif num % 3 == 0:
+        print(num, 'Fizz')
+    elif num % 5 == 0:
+        print(num, 'Buzz')
 
+# '''variant 3'''
+# def fizz_buzz(num):
+#     if num % 3 == 0:
+#         return 'Fizz'
+#     if num % 5 == 0:
+#         return 'Buzz'
+#     if num % 15 == 0:
+#         return 'FizzBuzz'
+#     '''type your number in fizz_buzz(n)'''
+# print(fizz_buzz(5))
