998ee065b580a67576196a5599e82ccc2079b59c
Akamichy <yanixjunior1@gmail.com>
2022-05-11 14:34:38 +0300

    domaha

--- 003_domaha.py
+++ 003_domaha.py
@@ -1,11 +1,31 @@
-range = range(1,101)
-for num in range:
-    if num % 15 == 0:
-        print(num)
-        print('FizzBuzz')
-    elif num % 3 == 0:
-        print(num)
-        print('Fizz')
-    elif num % 5 == 0:
-        print(num)
-        print('Buzz')
+# '''
+#
+# VARIANT 1
+#
+# '''
+
+# range = range(1,101)
+# for num in range:
+#     if num % 15 == 0:
+#         print(num)
+#         print('FizzBuzz')
+#     elif num % 3 == 0:
+#         print(num)
+#         print('Fizz')
+#     elif num % 5 == 0:
+#         print(num)
+#         print('Buzz')
+'''
+
+VARIANT 2
+
+'''
+# range = range(1, 101)
+# for num in range:
+#     if num % 15 == 0:
+#         print(num, 'FizzBuzz')
+#     elif num % 3 == 0:
+#         print(num, 'Fizz')
+#     elif num % 5 == 0:
+#         print(num, 'Buzz')
+
