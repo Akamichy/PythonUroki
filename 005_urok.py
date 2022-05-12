daf6bab60ebbb4275b730e6c84f69b73c37f4e87
Akamichy <yanixjunior1@gmail.com>
2022-05-12 18:32:19 +0300

    urok

---
+++ 005_urok.py
@@ -0,0 +1,16 @@
+'''Программа, которая находит простые числа и ищет их общую сумму'''
+# primes = []
+# n = int(input('Введите число N:'))
+# for num in range(1, n + 1):
+#     cnt = 0
+#     if num == 1:
+#         primes.append(num)
+#     for num1 in range(1, num + 1):
+#         if cnt > 2:
+#             break
+#         if num%num1 == 0:
+#             cnt += 1
+#     if cnt == 2:
+#         primes.append(num)
+# print(sum(primes))
+
