7e5ba7786bd1fd89231922fa53c2ae058cd34235
Akamichy <yanixjunior1@gmail.com>
2022-05-12 19:14:31 +0300

    urok

--- 005_urok.py
+++ 005_urok.py
@@ -13,4 +13,19 @@
 #     if cnt == 2:
 #         primes.append(num)
 # print(sum(primes))
-
+x = 5
+'''такие скобки создают словарь
+str:str - структура словаря, не разрешены дубликаты ключей внутри(они в левой позиции), значение может повторяться
+ключи можно переносить таким образом: {
+                                        }
+значение ключу присваивается двоеточием'''
+student = {'name':'John', 'age':32, 'courses':['Math', 'Biology', 'Art'], 1:'int_key', 2.0:'float_key',
+           x:'variable', 'Hello': x, 'y':{'new_name':'Mary', 'new_surname':'Smith'}}
+# print(student.keys())
+# print(student.values())
+# print(student.items())
+# print(list(student.items())[0])
+# for key in student.items():
+#     print(key[0])
+# for x in student:
+#     print(student[x])
